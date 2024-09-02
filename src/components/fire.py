ded40f344e006a983c42de2318856046ad0177ab
후니 <hipark@huniui-MacBookPro.local>
2024-09-02 14:21:02 +0900

    updateBuildConfig

--- src/components/fire.py
+++ src/components/fire.py
@@ -13,8 +13,8 @@ def initialize_firebase():
         current_dir = os.path.dirname(os.path.abspath(__file__))
     
     # JSON 파일 경로를 설정합니다.
-    # json_path = os.path.join(current_dir, 'test-hungun-firebase-adminsdk-rl8wp-7e30142f0f.json')
-    json_path = os.path.join(current_dir, 'dymfsys-firebase-adminsdk-3fkh3-957df8a288.json')
+    json_path = os.path.join(current_dir, 'test-hungun-firebase-adminsdk-rl8wp-7e30142f0f.json')
+    # json_path = os.path.join(current_dir, 'dymfsys-firebase-adminsdk-3fkh3-957df8a288.json')
     
     if not os.path.exists(json_path):
         raise FileNotFoundError(f"JSON file not found: {json_path}")
@@ -22,8 +22,8 @@ def initialize_firebase():
     cred = credentials.Certificate(json_path)
 
     firebase_admin.initialize_app(cred, {
-        # 'storageBucket': 'test-hungun.appspot.com'  # Firestore Storage 주소
-        'storageBucket': 'dymfsys.appspot.com'  # Firestore Storage 주소
+        'storageBucket': 'test-hungun.appspot.com'  # Firestore Storage 주소
+        # 'storageBucket': 'dymfsys.appspot.com'  # Firestore Storage 주소
     })
 
     db = firestore.client()
